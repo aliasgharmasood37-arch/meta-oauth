211f862d1e6a66e5887d7b5af20fcccbaeb069f2
Aliasghar Masood <aliasgharmasood37@gmail.com>
2026-02-21 02:50:21 +0530

    aadded reauthorize auth type in connect

--- app/auth/connect/route.ts
+++ app/auth/connect/route.ts
@@ -4,8 +4,10 @@ export async function GET() {
   const params = new URLSearchParams({
     client_id: process.env.META_APP_ID!,
     redirect_uri: `${process.env.BASE_URL}/auth/callback`,
-    scope: "pages_show_list,instagram_basic,instagram_manage_messages",
+    scope: "pages_show_list,instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments",
     response_type: "code",
+    auth_type: "reauthorize",
+    display: "popup",
   });
 
   const url = `https://www.facebook.com/v19.0/dialog/oauth?${params.toString()}`;
